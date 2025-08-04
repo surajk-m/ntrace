e60ccfd1ed6173e8b282777b624e4a59ca7f9fbc
surajk-m <suraj410401@gmail.com>
2025-08-04 01:25:50 +0000

    chore(release): Bump to `v0.1.4` and Update changelog

--- src/main.rs
+++ src/main.rs
@@ -199,7 +199,7 @@ fn print_banner() {
     println!(
         "{} {} {}",
         "║".blue().bold(),
-        " Network Port Scanner & Protocol Analyzer v0.1.3 "
+        " Network Port Scanner & Protocol Analyzer v0.1.4 "
             .on_bright_blue()
             .white()
             .bold(),
@@ -269,7 +269,7 @@ fn print_trace_banner() {
     println!(
         "{} {} {}",
         "║".blue().bold(),
-        " Traceroute & Network Protocol Analyzer v0.1.3 "
+        " Traceroute & Network Protocol Analyzer v0.1.4 "
             .on_bright_blue()
             .white()
             .bold(),
